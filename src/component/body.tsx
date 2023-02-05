1d368c1a8cdb1ce9bacb0c1164ed0525b0a9c43f
dorteks <ayodelealayemi@gmail.com>
2023-02-05 13:28:42 +0100

    added responsiveness to main page

--- src/component/body.tsx
+++ src/component/body.tsx
@@ -12,40 +12,47 @@ import React from "react";
 
 const Body = () => {
   return (
-    <Grid templateColumns="repeat(2, 1fr)" gap={20}>
+    <Grid
+      templateColumns="repeat(2, 1fr)"
+      pl={["11rem", "11rem", "11rem", "0rem", "11rem"]}
+      pr={["11rem", "11rem", "11rem", "0rem", "11rem"]}
+      pt="5rem"
+      maxWidth="100vw"
+      maxHeight="100vh"
+    >
       <GridItem minWidth="auto" height="auto">
         <Stack align="center">
           <Img
             borderRadius="20px"
-            boxSize="450px"
+            boxSize={["28rem", "28rem", "28rem", "24rem", "28rem"]}
             src="/images/image-product-1.jpg"
           />
-          <HStack gap={9} pt="40px">
+          <HStack pt="40px">
             <Img
               borderRadius="15px"
-              boxSize="100px"
+              boxSize={["0rem", "0rem", "0rem", "5.6rem", "6.3rem"]}
               src="/images/image-product-1-thumbnail.jpg"
             />
             <Img
               borderRadius="15px"
-              boxSize="100px"
+              boxSize={["0rem", "0rem", "0rem", "5.6rem", "6.3rem"]}
               src="/images/image-product-2-thumbnail.jpg"
             />
             <Img
               borderRadius="15px"
-              boxSize="100px"
+              boxSize={["0rem", "0rem", "0rem", "5.6rem", "6.3rem"]}
               src="/images/image-product-3-thumbnail.jpg"
             />
             <Img
               borderRadius="15px"
-              boxSize="100px"
+              boxSize={["0rem", "0rem", "0rem", "5.6rem", "6.3rem"]}
               src="/images/image-product-4-thumbnail.jpg"
             />
           </HStack>
         </Stack>
       </GridItem>
-      <GridItem width="450px" height="auto">
-        <Stack mt="4rem">
+      <GridItem width="100%" height="auto">
+        <Stack mt="4rem" ml="2rem" width="500px">
           <Text letterSpacing={1} fontWeight="bold" color="orange">
             SNEAKER COMPANY
           </Text>
@@ -70,23 +77,24 @@ const Body = () => {
           <Text as="s" pt="0.5rem" fontWeight="bold">
             $250.00
           </Text>
-          <HStack pt="2rem" gap={10}>
+          <HStack pt="2rem">
             <Button
               width="180px"
               backgroundColor="gray.50"
               borderColor="white"
               height="60px"
-              gap={40}
               borderRadius="15px"
             >
               <Img src="/images/icon-minus.svg" />
-              <Text fontSize="1rem">0</Text>
+              <Text fontSize="1rem" pl="3rem" pr="3rem">
+                0
+              </Text>
               <Img src="/images/icon-plus.svg" />
             </Button>
             <Button
               width="300px"
               height="60px"
-              gap={20}
+              gap={5}
               borderColor="white"
               borderRadius="15px"
               fontSize="1rem"
