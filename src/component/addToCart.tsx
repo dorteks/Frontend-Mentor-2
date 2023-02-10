086bb71cfd8507c79c4fa49b9d52c538cec7c6bd
dorteks <ayodelealayemi@gmail.com>
2023-02-10 20:16:03 +0100

    restructured files, added checkout component

---
+++ src/component/addToCart.tsx
@@ -0,0 +1,62 @@
+import { Stack, Button, Img, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const AddToCart = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Stack
+      direction={["column", "column", "row", "row", "row"]}
+      pt={["2rem", "1.5rem", "1rem", "1.5rem", "2rem"]}
+      gap={["5", "5", "0", "0", "0"]}
+    >
+      <Stack
+        direction="row"
+        borderColor="white"
+        align="center"
+        justify="space-between"
+        borderRadius="15px"
+        gap={[0, 0, 7, 9, 9]}
+        backgroundColor="gray.50"
+        height={["60px", "60px", "50px", "60px", "60px"]}
+        width={["auto", "auto", "180px", "180px", "180px"]}
+      >
+        <Button
+          backgroundColor="gray.50"
+          isDisabled={count === 0}
+          onClick={() => setCount(count - 1)}
+        >
+          <Img src="/images/icon-minus.svg" />
+        </Button>
+        <Text fontSize="1rem" width="50px" textAlign="center">
+          {count}
+        </Text>
+        <Button
+          backgroundColor="gray.50"
+          disabled={count < 2}
+          onClick={() => setCount(count + 1)}
+        >
+          <Img
+            src="/images/icon-plus.svg"
+            onClick={() => setCount(count + 1)}
+          />
+        </Button>
+      </Stack>
+      <Button
+        gap={5}
+        fontSize="1rem"
+        fontWeight="bold"
+        borderColor="white"
+        borderRadius="15px"
+        backgroundColor="orange"
+        height={["60px", "60px", "50px", "60px", "60px"]}
+        width={["auto", "auto", "300px", "300px", "300px"]}
+      >
+        <Img src="/images/icon-cart.svg" />
+        Add to Cart
+      </Button>{" "}
+    </Stack>
+  );
+};
+
+export default AddToCart;
--- src/component/body/mobilebody.tsx
+++ src/component/body/mobilebody.tsx
@@ -1,9 +1,8 @@
-import { Box, Button, HStack, Img, Stack, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Box, HStack, Img, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+import AddToCart from "../addToCart";
 
 const MobileBody = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <Stack display={["flex", "flex", "none", "none", "none"]}>
       <Stack
@@ -107,51 +106,7 @@ const MobileBody = () => {
           </Text>
         </HStack>
 
-        <Stack pt="40px" gap={3}>
-          <Stack
-            width="full"
-            height="60px"
-            align="center"
-            direction="row"
-            borderColor="white"
-            borderRadius="15px"
-            justify="space-between"
-            backgroundColor="gray.50"
-          >
-            <Button
-              backgroundColor="gray.50"
-              isDisabled={count === 0}
-              onClick={() => setCount(count - 1)}
-            >
-              <Img src="/images/icon-minus.svg" />
-            </Button>
-            <Text fontSize="1rem" width="50px" textAlign="center">
-              {count}
-            </Text>
-            <Button
-              backgroundColor="gray.50"
-              onClick={() => setCount(count + 1)}
-            >
-              <Img
-                src="/images/icon-plus.svg"
-                onClick={() => setCount(count + 1)}
-              />
-            </Button>
-          </Stack>
-          <Button
-            gap={5}
-            fontSize="1rem"
-            fontWeight="bold"
-            borderColor="white"
-            borderRadius="15px"
-            backgroundColor="orange"
-            height="60px"
-            width="full"
-          >
-            <Img src="/images/icon-cart.svg" />
-            Add to Cart
-          </Button>
-        </Stack>
+        <AddToCart />
       </Stack>
     </Stack>
   );
